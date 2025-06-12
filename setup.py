da76e31a88e05cf20068afa8cd1d52f2c921672b
Choin72 <choin22222@naver.com>
2025-06-12 18:20:40 +0900

    setup.py | data_files updated

--- setup.py
+++ setup.py
@@ -9,10 +9,9 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        #('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/resource', glob.glob('resource/*')),
-        ('share/' + package_name + '/resource', glob.glob('resource/.env')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
